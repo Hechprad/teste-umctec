29f68d9fbd88f5efb06fbfaa336a2c356b732587
Jorge Hecherat <hecherat@gmail.com>
2020-10-03 16:54:59 -0300

    [develop] - update Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { Redirect, Route, RouteProps, Switch } from 'react-router-dom'
 
 import Activities from 'pages/Activities'
+import NotFound from 'pages/NotFound'
 
 const Routes: React.FC<RouteProps> = () => (
   <Switch>
     <Route path="/" exact render={() => <Redirect to="/activities" />} />
     <Route path="/activities" exact component={Activities} />
+    <Route path="/accounts" exact component={NotFound} />
+    <Route path="/users" exact component={NotFound} />
+    <Route path="/performance" exact component={NotFound} />
+    <Route path="/dashboard" exact component={NotFound} />
+    <Route component={NotFound} />
   </Switch>
 )
 
